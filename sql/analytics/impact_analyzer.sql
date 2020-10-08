8d2ead212b9c8dd94434fe72d5768beb557f51e2
Sebastian Dreßler <sebastian@swarm64.com>
2020-10-08 11:51:31 +0200

    Add initial impact analyzer SQL

---
+++ sql/analytics/impact_analyzer.sql
@@ -0,0 +1,174 @@
+DROP FUNCTION IF EXISTS _calculate_cache_ratio(NUMERIC, NUMERIC);
+CREATE FUNCTION _calculate_cache_ratio(sum_blks_read NUMERIC, sum_blks_hit NUMERIC)
+RETURNS DOUBLE PRECISION AS $$
+  SELECT COALESCE(
+    ROUND(
+      sum_blks_hit / NULLIF((sum_blks_read + sum_blks_hit), 0),
+    2), 0.0
+  )::DOUBLE PRECISION
+$$ LANGUAGE SQL PARALLEL SAFE;
+
+DROP FUNCTION IF EXISTS _within_range(TIMESTAMPTZ, TIMESTAMPTZ, INTERVAL, INTERVAL);
+CREATE FUNCTION _within_range(t1 TIMESTAMPTZ, t2 TIMESTAMPTZ, lower INTERVAL, upper INTERVAL)
+RETURNS BOOL AS $$
+  SELECT CASE
+    WHEN lower IS NOT NULL AND upper IS NOT NULL THEN (t2 - t1 >= lower) AND (t2 - t1) < upper
+    WHEN lower IS NULL AND upper IS NOT NULL THEN (t2 - t1) < upper
+    WHEN lower IS NOT NULL AND upper IS NULL THEN (t2 - t1) >= lower
+  END
+$$ LANGUAGE SQL PARALLEL SAFE;
+
+DROP PROCEDURE IF EXISTS _s64da_impact_analyzer_print();
+CREATE PROCEDURE _s64da_impact_analyzer_print() AS $$
+DECLARE
+  row RECORD;
+BEGIN
+  FOR row IN SELECT
+      COUNT(*) AS num_queries
+    , SUM(CASE WHEN _within_range(query_start, query_stop, NULL, '60s'::INTERVAL) THEN 1 ELSE 0 END) AS fast
+    , SUM(CASE WHEN _within_range(query_start, query_stop, '60s'::INTERVAL, '15min'::INTERVAL) THEN 1 ELSE 0 END) AS medium
+    , SUM(CASE WHEN _within_range(query_start, query_stop, '15min'::INTERVAL, NULL) THEN 1 ELSE 0 END) AS slow
+    , COALESCE(MAX(max_parallelism), 0) AS max_parallelism
+    , COALESCE(MIN(max_parallelism), 0) AS min_parallelism
+    , COALESCE(ROUND(AVG(max_parallelism)), 0) AS avg_parallelism
+  FROM query_stats LOOP
+    RAISE NOTICE '%', to_json(row);
+  END LOOP;
+
+  FOR row IN SELECT
+    SUM(heap_blks_read) AS heap_disk
+  , SUM(heap_blks_hit) AS heap_cache
+  , _calculate_cache_ratio(SUM(heap_blks_read), SUM(heap_blks_hit)) AS heap_ratio
+  , SUM(idx_blks_read) AS idx_disk
+  , SUM(idx_blks_hit) AS idx_cache
+  , _calculate_cache_ratio(SUM(idx_blks_read), SUM(idx_blks_hit)) AS idx_ratio
+--  , SUM(toast_blks_read - toast_blks_read_base) AS toast_disk
+--  , SUM(toast_blks_hit - toast_blks_hit_base) AS toast_cache
+--  , _calculate_cache_ratio(SUM(toast_blks_read), SUM(toast_blks_hit)) AS toast_ratio
+  , SUM(inserts) AS inserts
+  , SUM(updates) AS updates
+  , SUM(deletes) AS deletes
+    FROM workload_stats LOOP
+    RAISE NOTICE '%', to_json(row);
+  END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP PROCEDURE IF EXISTS s64da_impact_analyzer();
+CREATE PROCEDURE s64da_impact_analyzer() AS $$
+DECLARE
+  own_datname VARCHAR;
+  own_pid INT;
+BEGIN
+  SELECT current_database() INTO own_datname;
+  SELECT pg_backend_pid() INTO own_pid;
+
+  RAISE NOTICE 'S64 DA Impact Analyzer on DB: %s', own_datname;
+
+  DROP TABLE IF EXISTS query_stats;
+  CREATE TEMP TABLE query_stats(
+      pid INT
+    , max_parallelism INT
+    , query_start TIMESTAMPTZ
+    , query_stop TIMESTAMPTZ
+    , runtime_parallel INTERVAL
+  );
+  ALTER TABLE query_stats ADD PRIMARY KEY(pid, query_start);
+
+  DROP TABLE IF EXISTS base_values;
+  CREATE TEMP TABLE base_values AS
+  SELECT
+      relid
+    , heap_blks_read
+    , heap_blks_hit
+    , idx_blks_read
+    , idx_blks_hit
+    , toast_blks_read
+    , toast_blks_hit
+    , n_tup_ins
+    , n_tup_upd
+    , n_tup_del
+  FROM pg_statio_user_tables stat_io
+  JOIN pg_stat_user_tables stat USING(relid)
+  WHERE stat_io.schemaname NOT LIKE 'pg_temp%'
+  ;
+
+  DROP TABLE IF EXISTS workload_stats;
+  CREATE TEMP TABLE workload_stats AS
+  SELECT
+      stat_io.relid
+    , stat_io.schemaname
+    , stat_io.relname
+    , 0::BIGINT AS heap_blks_read
+    , 0::BIGINT AS heap_blks_hit
+    , 0::BIGINT AS idx_blks_read
+    , 0::BIGINT AS idx_blks_hit
+    , 0::BIGINT AS toast_blks_read
+    , 0::BIGINT AS toast_blks_hit
+    , 0::BIGINT AS inserts
+    , 0::BIGINT AS updates
+    , 0::BIGINT AS deletes
+  FROM pg_statio_user_tables stat_io
+  JOIN pg_stat_user_tables stat USING(relid)
+  WHERE stat_io.schemaname NOT LIKE 'pg_temp%';
+
+  LOOP
+    BEGIN
+      DROP TABLE IF EXISTS current_activity;
+      CREATE TEMP TABLE current_activity AS
+      SELECT
+          query_start
+        , MIN(pid) AS parent_pid
+        , COUNT(*) AS worker_count
+      FROM pg_stat_activity
+      WHERE datname = own_datname
+        AND pid <> own_pid
+        AND state = 'active'
+      GROUP BY query_start;
+
+      INSERT INTO query_stats(query_start, pid, max_parallelism, runtime_parallel)
+      SELECT *, '0'::INTERVAL FROM current_activity
+      ON CONFLICT DO NOTHING
+      ;
+
+      UPDATE query_stats qs
+      SET runtime_parallel =
+            CASE
+              WHEN ca.worker_count > 1 THEN runtime_parallel + '1'::INTERVAL
+              ELSE runtime_parallel
+            END
+        , query_stop = NOW()
+      FROM current_activity ca
+      WHERE qs.pid = ca.parent_pid AND qs.query_start = ca.query_start
+      ;
+
+      UPDATE workload_stats ws
+      SET heap_blks_read = src.heap_blks_read - bv.heap_blks_read
+        , heap_blks_hit = src.heap_blks_hit - bv.heap_blks_hit
+        , idx_blks_read = src.idx_blks_read - bv.idx_blks_read
+        , idx_blks_hit = src.idx_blks_hit - bv.idx_blks_hit
+        , toast_blks_read = src.toast_blks_read - bv.toast_blks_read
+        , toast_blks_hit = src.toast_blks_hit - bv.toast_blks_hit
+      FROM pg_statio_user_tables src
+      JOIN base_values bv USING(relid)
+      WHERE ws.relid = src.relid;
+
+      UPDATE workload_stats ws
+      SET inserts = src.n_tup_ins - bv.n_tup_ins
+        , updates = src.n_tup_upd - bv.n_tup_upd
+        , deletes = src.n_tup_del - bv.n_tup_del
+      FROM pg_stat_user_tables src
+      JOIN base_values bv USING(relid)
+      WHERE ws.relid = src.relid;
+
+      CALL _s64da_impact_analyzer_print();
+      PERFORM pg_sleep(1);
+
+    EXCEPTION WHEN operator_intervention THEN
+      EXIT;
+    END;
+
+    COMMIT;
+  END LOOP;
+END;
+$$ LANGUAGE plpgsql;
